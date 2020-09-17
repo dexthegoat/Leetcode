5fc605986fd6c61405a85a5a8f8442dd1da64334
Jianshuo Yang <jyang63@stevens.edu>
2020-09-17 18:26:47 -0400

    Updated.

---
+++ src/WordBreak.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+ * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+ *
+ * Note:
+ *
+ * The same word in the dictionary may be reused multiple times in the segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * Example 1:
+ *
+ * Input: s = "leetcode", wordDict = ["leet", "code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ * Example 2:
+ *
+ * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ *              Note that you are allowed to reuse a dictionary word.
+ * Example 3:
+ *
+ * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output: false
+ */
+public class WordBreak {
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        // substring [a,b)
+        System.out.println(s.substring(4, 8));
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("leet");
+        wordDict.add("code");
+        System.out.println(new WordBreak().wordBreak(s, wordDict));
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        boolean[] flag = new boolean[s.length() + 1];
+        flag[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.println(s.substring(j, i));
+                if (flag[j] && wordDict.contains(s.substring(j, i))) {
+                    flag[i] = true;
+                    break;
+                }
+            }
+        }
+        return flag[s.length()];
+    }
+}
